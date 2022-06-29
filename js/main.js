f13c748c55d5f0ac8bae5170391f50383b4e18a7
Ajay-Singh-Dabi <ajaydabi20@gmail.com>
2022-06-29 19:31:00 +0530

    Completed the page

--- js/main.js
+++ js/main.js
@@ -18,4 +18,11 @@ $(document).ready(function(){
         nav:true,
         navText:[$('.owl-navigation .owl-nav-prev'),$('.owl-navigation .owl-nav-next')]
     });
+
+    //click to srcoll to the top
+    $('.move-up span').click(function(){
+        $('html, body').animate({
+            scrollTop:0
+        },1500);
+    });
 });
