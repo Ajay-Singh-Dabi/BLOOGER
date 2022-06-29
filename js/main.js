bb3f13087d2617277dea781fe4e4df65e58c74d9
Ajay-Singh-Dabi <ajaydabi20@gmail.com>
2022-06-29 15:12:32 +0530

    Blog Content

--- js/main.js
+++ js/main.js
@@ -7,4 +7,15 @@ $(document).ready(function(){
     $toggleCollapse.click(function(){
         $nav.toggleClass('collapse');
     })
+
+
+    //owl-carousel for blog
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        autoplay:true,
+        autoplayTimeout:3000,
+        dots:false,
+        nav:true,
+        navText:[$('.owl-navigation .owl-nav-prev'),$('.owl-navigation .owl-nav-next')]
+    });
 });
