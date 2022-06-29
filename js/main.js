418a767964dfd2b640fb6c44493bbf73b253c0c4
Ajay-Singh-Dabi <ajaydabi20@gmail.com>
2022-06-29 19:41:35 +0530

    Site-Responsiveness

--- js/main.js
+++ js/main.js
@@ -1,3 +1,19 @@
+const responsive={
+    0: {
+        items:1
+    },
+    
+    320:{
+        items:1
+    },
+    560: {
+        items:2
+    },
+    960: {
+        items:3
+    }
+}
+
 $(document).ready(function(){
 
     $nav = $('.nav');
@@ -16,7 +32,8 @@ $(document).ready(function(){
         autoplayTimeout:3000,
         dots:false,
         nav:true,
-        navText:[$('.owl-navigation .owl-nav-prev'),$('.owl-navigation .owl-nav-next')]
+        navText:[$('.owl-navigation .owl-nav-prev'),$('.owl-navigation .owl-nav-next')],
+        responsive: responsive
     });
 
     //click to srcoll to the top
